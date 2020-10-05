b9ada99a590e1a99b8bacb2d3b9ac4fef96b08d7
Sach Samala <sachit478@gmail.com>
2020-10-05 14:40:14 -0700

    changes

--- server.js
+++ server.js
@@ -6,17 +6,11 @@ const shortid = require("shortid");
 const app = express();
 app.use(bodyParser.json());
 
-app.use("/", express.static(__dirname + "/build"));
-app.get("/", (req, res) => res.sendFile(__dirname + "/build/index.html"));
-
-mongoose.connect(
-  process.env.MONGODB_URL || "mongodb://localhost/react-shopping-cart-db",
-  {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useUnifiedTopology: true,
-  }
-);
+mongoose.connect("mongodb://localhost/react-shopping-cart-db", {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
 
 const Product = mongoose.model(
   "products",
@@ -44,55 +38,6 @@ app.post("/api/products", async (req, res) => {
 app.delete("/api/products/:id", async (req, res) => {
   const deletedProduct = await Product.findByIdAndDelete(req.params.id);
   res.send(deletedProduct);
-});
-
-const Order = mongoose.model(
-  "order",
-  new mongoose.Schema(
-    {
-      _id: {
-        type: String,
-        default: shortid.generate,
-      },
-      email: String,
-      name: String,
-      address: String,
-      total: Number,
-      cartItems: [
-        {
-          _id: String,
-          title: String,
-          price: Number,
-          count: Number,
-        },
-      ],
-    },
-    {
-      timestamps: true,
-    }
-  )
-);
-
-app.post("/api/orders", async (req, res) => {
-  if (
-    !req.body.name ||
-    !req.body.email ||
-    !req.body.address ||
-    !req.body.total ||
-    !req.body.cartItems
-  ) {
-    return res.send({ message: "Data is required." });
-  }
-  const order = await Order(req.body).save();
-  res.send(order);
-});
-app.get("/api/orders", async (req, res) => {
-  const orders = await Order.find({});
-  res.send(orders);
-});
-app.delete("/api/orders/:id", async (req, res) => {
-  const order = await Order.findByIdAndDelete(req.params.id);
-  res.send(order);
 });
 
 const port = process.env.PORT || 5000;
--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,40 +1,21 @@
 import React, { Component } from "react";
-import { connect } from "react-redux";
-import { filterProducts, sortProducts } from "../actions/productActions";
 
-class Filter extends Component {
+export default class Filter extends Component {
   render() {
-    return !this.props.filteredProducts ? (
-      <div>Loading...</div>
-    ) : (
+    return (
       <div className="filter">
-        <div className="filter-result">
-          {this.props.filteredProducts.length} Products
-        </div>
+        <div className="filter-result">{this.props.count} Products</div>
         <div className="filter-sort">
           Order{" "}
-          <select
-            value={this.props.sort}
-            onChange={(e) =>
-              this.props.sortProducts(
-                this.props.filteredProducts,
-                e.target.value
-              )
-            }
-          >
-            <option value="latest">Latest</option>
+          <select value={this.props.sort} onChange={this.props.sortProducts}>
+            <option>Latest</option>
             <option value="lowest">Lowest</option>
             <option value="highest">Highest</option>
           </select>
         </div>
         <div className="filter-size">
           Filter{" "}
-          <select
-            value={this.props.size}
-            onChange={(e) =>
-              this.props.filterProducts(this.props.products, e.target.value)
-            }
-          >
+          <select value={this.props.size} onChange={this.props.filterProducts}>
             <option value="">ALL</option>
             <option value="XS">XS</option>
             <option value="S">S</option>
@@ -48,15 +29,3 @@ class Filter extends Component {
     );
   }
 }
-export default connect(
-  (state) => ({
-    size: state.products.size,
-    sort: state.products.sort,
-    products: state.products.items,
-    filteredProducts: state.products.filteredItems,
-  }),
-  {
-    filterProducts,
-    sortProducts,
-  }
-)(Filter);
